4ac45a8b853bb333636d78e4249feb0e77e78ac5
ClasherHulk <krnskrwr4@gmail.com>
2021-03-16 15:54:57 +0530

    final commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExCoreDbFx.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
